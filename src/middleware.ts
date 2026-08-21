9d32a73035fd82f0c35046ad96fc529082aa02f9
Ahmad Alshomaree <220208706@ostimteknik.edu.tr>
2026-08-22 02:33:14 +0300

    Aug 22 versio 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { type NextRequest } from "next/server";
 import { createClient } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return createClient(request);
+  return await createClient(request);
 }
 
 export const config = {
@@ -12,7 +12,7 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
+     * - Public static assets
      */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
